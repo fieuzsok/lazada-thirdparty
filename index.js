217e42889eb4eda18c4bfc1885f9e79421636d64
ThaoLa <nhatchon56@gmail.com>
2020-10-17 20:15:01 +0700

    - init collect accessToken

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const http = require("http");
+const lazada_api_call = require("./lazada_api_call");
+const express = require("express");
+const bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+var server = http.createServer(app);
+app.get("/", (req, res) => {
+  //res.send(req);
+  var result = lazada_api_call.demoCallApi();
+  res.send(result);
+});
+
+app.post(
+  '/getAccessToken',
+  (req,res) => {
+    console.log(`statusCode: ${req.statusCode}`)
+    if(req.body)
+   {
+    console.log(req.body)
+    res.send(req.body);
+   } 
+  }
+)
+
+server.listen(3000, () => {
+  console.log("Server Start on 3000!!");
+});
