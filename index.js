1ad2d8541b23a6a935baf529e1c1e8afdea761b9
ThaoLa <Thao.La@gbst.com>
2020-11-19 21:23:26 +0700

    - rm un used

--- index.js
+++ index.js
@@ -33,20 +33,6 @@ app.get(
   }
 )
 
-// // function get UserToken
-// app.get(
-//   '/orders',
-//   (req,res) => {
-//     dbconnect.dbconnect();
-//      if(req.query && req.query.createdAfter || req.query.updateAfter){
-//       const { createdAfter, updateAfter } = req.query;
-//       orders.getOrders('testdeco02@mailinator.com', { created_after: createdAfter, update_after: updateAfter }).then((value)=>{
-//         res.send(value)
-//     });
-//      }
-    
-//   }
-// )
 
 const orderGraph = graphqlHTTP({
     schema: schema,
